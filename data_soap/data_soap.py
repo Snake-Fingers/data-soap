1aeacb47973f1688ad470694caf7340de3112c87
Choi702 <jaeychoi1@gmail.com>
2021-02-15 22:01:22 -0800

    pull comma

--- data_soap/data_soap.py
+++ data_soap/data_soap.py
@@ -1,9 +1,48 @@
-def soap():
+import pandas as pd 
+import numpy as np 
+import re   
+
+def soap(data):
+
     """Pulls trailing and leading character
     """
-    pass
+    # identify columns that need cleaning
+    # return a copy of input of the values properly converted
+    # pass
 
 def show_diff():
     """Shows both the original and cleaned dataframes.
     """
-    pass 
+    # pass 
+
+def pull_comma(line: str)-> str:
+    line = line.split()
+    for char in line:
+        if char == ',':
+            line.pop(',')
+         
+        else:
+            continue 
+
+    return ''.join(line) 
+            
+
+      # pass
+
+def pull_leading_character():
+    pass
+
+def pull_trailing_character():
+    pass
+
+def convert_all():
+    pass
+
+
+
+# define methods for pulling commas out of a String
+
+
+# define methods for pulling leading characters
+# define methods for pulling trailing characters
+# identify highest denomination and convert all figures to fraction of that denomination
