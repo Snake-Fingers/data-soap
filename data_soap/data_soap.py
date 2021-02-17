1a6bee1996fcdeaa6a199e900f113959ca38a5ef
MasonChance <mason.fryberger@gmail.com>
2021-02-16 21:39:28 -0800

    tests for soap() method written. soap() outlined not yet passing"

--- data_soap/data_soap.py
+++ data_soap/data_soap.py
@@ -2,18 +2,22 @@ import pandas as pd
 import numpy as np 
 import re   
 
-def soap(data):
+def soap(data, dirty:list):
 
     """Pulls trailing and leading character
     """
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError(f'TypeError: expected pd.DataFrame object, pd.Series object, or list-like: got {type(data)}')
     # create copy of the dataframe to be cleaned
-    # identify columns that need cleaning
-        #user entered, required parameter. accepts list of strings
+    clean_data = data.copy()
+    for col in dirty:
+        clean_data[f'{col}'].replace(clean_data[f'{col}'].values, [pd.to_numeric(pull_trailing_character(pull_leading_character(pull_comma(val)))) for val in clean_data[f'{col}']], inplace=True)
     # run pd.DataFrame.replace for all indicated columns on the copy of the df input.
         # applies all formatter functions defined below to the 
         # columns replacing the values with the return of those funcitons
             # Takes form: ` df['column to be formatted'].replace(df[column to be formatted].values, [pd.to_numeric(callback_1(callback_2(val))) for val in df['collumn to be formatted']], inplace=True)` 
     # return a copy of input of the values properly converted
+    return clean_data
     # pass
 
 def show_diff():
@@ -32,18 +36,18 @@ def show_diff():
 
 # define methods for pulling commas out of a String
 def pull_comma(line: str)-> str:
-  """For use on integer||float values represented by strings containing "," characters.
+    """For use on integer||float values represented by strings containing "," characters.
      Does not alter the notation or denomination of the value. ie: "1,000" will not become
      "1k" 
      input <-- str
      output --> str
-  """
-  if ',' in line:
-    line = line.split(',')
-    line = ''.join(line)
-    return line
-  else:
-    return line
+    """
+    if ',' in line:
+        line = line.split(',')
+        line = ''.join(line)
+        return line
+    else:
+        return line
             
 
 # define methods for pulling leading characters
@@ -55,11 +59,11 @@ def pull_trailing_character(line):
 
      input<-- str
      output--> str
-  """
-  if not line.isnumeric():
-    return line[:-1] if line[0].isnumeric() else 'Nan'
-  else:
-    return line
+    """
+    if not line.isnumeric():
+        return line[:-1] if line[0].isnumeric() else 'Nan'
+    else:
+        return line
 
     # current solution assumes that leading chars will always be in the form: `$xx.xx` with no additional whitespaces or chars between 
     # the char in question and the numeric string chars we actually want. mvp: keep assumption, note it in Docs. stretch: account for other possibilities
@@ -69,10 +73,10 @@ def pull_trailing_character(line):
 
 # define methods for pulling trailing characters
 def pull_leading_character():
-     """For use on numeric strings that begin with a currency or denom char.
+    """For use on numeric strings that begin with a currency or denom char.
 
-  """
-  return cvs_str[1:] if cvs_str[1].isnumeric() else 0
+    """
+    return cvs_str[1:] if cvs_str[1].isnumeric() else 0
     # current solution assumes trailing char only == 'm || M' or 'k || K' and converts 'k || K' to a decimal of 1Million. 
     # mvp: keep assumption and note it in Docs. stretch: account for all unit conversion types.
     # current solution takes form ` if str[-1] == 'k': convert to str[:-1]//100^10 else return str[:-1]`
--- tests/test_data_soap.py
+++ tests/test_data_soap.py
@@ -18,26 +18,54 @@ def sample_dataframe():
 #     df = sample_dataframe()
 #     print(df.info)
 # @pytest.mark.skip('pending code')
-def test_pull_comma():
-    actual = pull_comma('1,000,000')
+def test_pull_comma_when_trail_char():
+    actual = pull_comma('1,000,000+')
     expected = '1000000+'
     assert actual == expected
 
-@pytest.mark.skip('pending code')
-def test_pull_leading_character(sample_dataframe):
-    df = sample_dataframe
-    actual = pull_leading_character(df['Price'].iloc[236])
-    expected = '4.99'
+# @pytest.mark.skip('pending code')
+def test_pull_comma_when_lead_char():
+    actual = pull_comma('$1,000,000')
+    expected = '$1000000'
     assert actual == expected
 
-@pytest.mark.skip('pending code')
-def test_pull_trailing_character(sample_dataframe):
-    df = sample_dataframe
-    actual = test_pull_trailing_character(df['Installs'].iloc[232])
-    expected = '1,000,000'
+# @pytest.mark.skip('pending code')
+def test_pull_comma_when_no_comma():
+    actual = pull_comma('1000000')
+    expected = '$1000000'
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+
+# @pytest.mark.skip('pending code')
+def test_pull_comma_when_NaN():
+    actual = pull_comma('this, is NaN')
+    expected = 'this, is Nan'
+    assert actual == expected
+
+
+# @pytest.mark.skip('pending code')
+def test_pull_leading_character():
+    df = sample_dataframe
+    actual = pull_leading_character('$4.99')
+    expected = 4.99
+    assert actual == expected
+
+# @pytest.mark.skip('pending code')
+def test_pull_trailing_character_m():
+    df = sample_dataframe
+    actual = pull_trailing_character('1000000m')
+    expected = 1
+    assert actual == expected
+
+# @pytest.mark.skip('pending code')
+def test_pull_trailing_character_k():
+    df = sample_dataframe
+    actual = pull_trailing_character('1,000k')
+    expected = .001
+    assert actual == expected
+
+# @pytest.mark.skip('pending code')
+# re-design needed convert takes any unit of measure and converts to common denomination of that measure e.g: x'k' to .x 'm' or x 'ml' to .x'lt' etc.
 def test_convert_all(sample_dataframe):
     df = sample_dataframe
     actual = test_convert_all(df['Size'].iloc[211])
@@ -46,9 +74,40 @@ def test_convert_all(sample_dataframe):
 
 
 # @pytest.mark.skip('pending code')
-# @pytest.mark.skip('pending code')
-# @pytest.mark.skip('pending code')
-# @pytest.mark.skip('pending code')
+def test_soap_one_column_clean_commas(sample_dataframe):
+    rinsed = soap(sample_dataframe, ['Installs'])
+    actual = rinsed.iloc[211]['Installs']
+    expected = .023
+    assert actual == expected
+
+
+@pytest.mark.skip('pending code')
+def test_soap_one_column_clean_trailing(sample_dataframe):
+    rinsed = soap(sample_dataframe, ['Reviews'])
+    actual = rinsed.iloc[200]['Reviews']
+    expected = 10000000
+    assert actual == expected
+
+
+@pytest.mark.skip('pending code')
+def test_soap_more_columns(sample_dataframe):
+    rinsed = soap(sample_dataframe, ['Reviews', 'Installs', 'Price'])
+    data_types = ['object', 'float64'*3]
+    actual = rinsed.dtypes.values
+    expected = data_types
+    assert actual == expected
+
+
+@pytest.mark.skip('pending code')
+def test_soap_wrong_input_type():
+    bad_input = 'not a dataframe or series'
+    with pytest.raises(Exception) as excinfo:
+        soap(bad_input, ['NaN'])
+    actual = excinfo.value
+    expected = 'TypeError: expected pd.DataFrame object, pd.Series object, or list-like: got str'
+    assert actual == expected
+
+
 # @pytest.mark.skip('pending code')
     
     
