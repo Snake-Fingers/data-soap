51cf263603fa9b6195673be4389f2bb9ea516062
Choi702 <jaeychoi1@gmail.com>
2021-02-20 10:12:14 -0800

    initial commit no change

--- tests/test_data_soap.py
+++ tests/test_data_soap.py
@@ -10,58 +10,54 @@ def test_version():
 def sample_dataframe():
     df = pd.read_csv('assets/googleplaystore.csv')
     df = pd.DataFrame(df.copy().drop(['Category', 'Rating', 'Type', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver'], axis = 1)).iloc[200:240]
-    # print(df.info())
     return df
     
 
-# def test_sample_test(sample_dataframe):
-#     df = sample_dataframe()
-#     print(df.info)
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_comma_when_trail_char():
     actual = pull_comma('1,000,000+')
     expected = '1000000+'
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_comma_when_lead_char():
     actual = pull_comma('$1,000,000')
     expected = '$1000000'
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_comma_when_no_comma():
     actual = pull_comma('$1000000')
     expected = '$1000000'
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_comma_when_NaN():
     actual = pull_comma('this, is NaN')
     expected = 'this is NaN'
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_leading_character():
     actual = pull_leading_character('$4.99')
     expected = '4.99'
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_trailing_character_m():
     actual = pull_trailing_character('1m')
     expected = 1.0
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_pull_trailing_character_k():
     actual = pull_trailing_character('1k')
     expected = .001
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 # re-design needed convert takes any unit of measure and converts to common denomination of that measure e.g: x'k' to .x 'm' or x 'ml' to .x'lt' etc.
 def test_convert_all(sample_dataframe):
     df = sample_dataframe
@@ -70,7 +66,7 @@ def test_convert_all(sample_dataframe):
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_soap_one_column_clean(sample_dataframe):
     rinsed = soap(sample_dataframe, ['Size'])
     actual = rinsed['Size'].iloc[9]
@@ -78,7 +74,7 @@ def test_soap_one_column_clean(sample_dataframe):
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_soap_more_columns(sample_dataframe):
     rinsed = soap(sample_dataframe, ['Reviews', 'Installs', 'Price', 'Size'])
     # rinsed.convert_dtypes()
@@ -100,7 +96,6 @@ def test_soap_wrong_input_type():
     assert actual == expected
 
 
-# @pytest.mark.skip('pending code')
 
 # @pytest.mark.skip('pending code')
 def test_convert_unit():
